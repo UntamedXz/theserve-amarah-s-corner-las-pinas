5460c0983b4bbec528629a66e53b8eefb9544972
Untamed Jenn <untamedandromeda@gmail.com>
2022-05-25 22:44:24 +0800

    05.25.22.v3

--- database/theserve-amarah-s-corner-db.sql
+++ database/theserve-amarah-s-corner-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 22, 2022 at 08:32 AM
+-- Generation Time: May 25, 2022 at 04:37 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `theserve-amarah-s-corner-bf-las-piñas-db`
+-- Database: `theserve-amarah-s-corner-db`
 --
-CREATE DATABASE IF NOT EXISTS `theserve-amarah-s-corner-bf-las-piñas-db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `theserve-amarah-s-corner-bf-las-piñas-db`;
+CREATE DATABASE IF NOT EXISTS `theserve-amarah-s-corner-db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `theserve-amarah-s-corner-db`;
 
 -- --------------------------------------------------------
 
@@ -60,17 +60,19 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`category_id`, `category_title`, `categoty_thumbnail`) VALUES
-(1, 'Pizza', '62864cd75adde-pizza2.png'),
+(1, 'Pizza', '628d243bf079e.png'),
 (2, 'Chicken Wings', '628655e6eb31f-wings.png'),
-(3, 'Pasta', '628656877eb10-pasta.png'),
+(3, 'Pasta ', '628d24768fdc2.png'),
 (4, 'Cheesy Snacks', '628656d4224d5-cheesy.png'),
 (5, 'Milk Tea', '62865c1cde79d-milktea.png'),
 (6, 'Fruit Tea', '62865c8836ff7-fruit.png'),
 (7, 'Lemonade', '62865e009307e-lemonade.png'),
-(8, 'Cheesecake Series', '62865e4831149-cheesecake.png'),
+(8, 'Cheesecake Series Lo', '62865e4831149-cheesecake.png'),
 (9, 'Milk Shake', '62865e78e81fe-milkshake.png'),
 (10, 'Hot & Cold Coffee', '62865e94d281a-hotCoffee.png'),
-(11, 'Frappe', '62865ec47fdb6-frappe.png');
+(11, 'Frappe', '62865ec47fdb6-frappe.png'),
+(13, 'Deputa', '628da82cc5f45.png'),
+(14, 'Dugago', '628da850d5265.png');
 
 -- --------------------------------------------------------
 
@@ -93,6 +95,25 @@ INSERT INTO `customers` (`user_id`, `username`, `email`, `password`) VALUES
 (1, 'KayeB', 'kaye.billones@cvsu.edu.ph', 'kayeeeb'),
 (2, 'untamed', 'untamedandromeda@gmail.com', 'untamed_jenn'),
 (3, 'markyyy', 'markryan.jancorda@cvsu.edu.ph', 'markyyyboy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategory`
+--
+
+CREATE TABLE `subcategory` (
+  `category_id` int(11) NOT NULL,
+  `subcategory_id` int(11) NOT NULL,
+  `subcategory_title` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `subcategory`
+--
+
+INSERT INTO `subcategory` (`category_id`, `subcategory_id`, `subcategory_title`) VALUES
+(1, 1, 'Classic Flavor');
 
 --
 -- Indexes for dumped tables
@@ -118,6 +139,13 @@ ALTER TABLE `customers`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD PRIMARY KEY (`subcategory_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -131,13 +159,29 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  MODIFY `subcategory_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD CONSTRAINT `subcategory_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
